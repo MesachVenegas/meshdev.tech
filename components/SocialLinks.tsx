69ad0641810b7381982669602906766e7eeb1467
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-11 01:16:35 -0800

    changes: target blank links

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -7,7 +7,7 @@ export default function SocialLinks() {
         {
           socialsLinks.map( ({name, icon, url}) => (
             <span key={name} className="hover:text-primary w-6 h-6">
-              <Link href={url} className="menu-icon" target="_blank">
+              <Link href={url} className="menu-icon" target="_blank" title={`${name} link icon`}>
                 {icon}
               </Link>
             </span>
