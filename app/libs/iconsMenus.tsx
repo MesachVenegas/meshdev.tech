9e6ba315637049c74f6a3cfa14b7be582e12f5d2
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-10 22:26:00 -0800

    add: social links icons

--- app/libs/iconsMenus.tsx
+++ app/libs/iconsMenus.tsx
@@ -1,3 +1,4 @@
+import { faGithub, faInstagram, faLinkedin, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { faAt, faBriefcase, faEarth, faGear, faHome, faMoon, faSun, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -45,5 +46,29 @@ export const settingsIcons = [
         icon: <FontAwesomeIcon icon={faGear}/>,
         desktop: true,
         mobile: true
+    },
+]
+
+
+export const socialsLinks = [
+    {
+        name: "github",
+        icon: <FontAwesomeIcon icon={faGithub}/>,
+        url: "https://github.com/MesachVenegas"
+    },
+    {
+        name: "linkedin",
+        icon: <FontAwesomeIcon icon={faLinkedin}/>,
+        url: "https://www.linkedin.com/in/mesachvenegas/"
+    },
+    {
+        name: "instagram",
+        icon: <FontAwesomeIcon icon={faInstagram}/>,
+        url: "https://www.instagram.com/mesh_dev/"
+    },
+    {
+        name: "whatsapp",
+        icon: <FontAwesomeIcon icon={faWhatsapp}/>,
+        url: "https://api.whatsapp.com/send?phone=5216631662698&text=Quiero%20trabajar%20contigo"
     },
 ]
