8a7c33a3c100124dc42e251f864773d5463f91dd
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-11 01:17:29 -0800

    changes: animate footer on view

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -1,13 +1,23 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
 
+import { motion } from "framer-motion";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import SocialLinks from "@/components/SocialLinks";
+import { fadeIn } from "@/app/libs/motionConfig";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-col gap-4 justify-center items-center py-6 bg-secondary/60 border-black border-1 backdrop-blur-sm">
+    <motion.footer
+      className="flex flex-col gap-4 justify-center items-center py-6 bg-secondary/60 border-black border-1 backdrop-blur-sm"
+      variants={ fadeIn('up', 0.3)}
+      initial="hidden"
+      animate="show"
+      exit="hidden"
+    >
       <Link href="/" className="flex gap-2 items-center justify-center h-16 w-[250px]">
         <Image src="/assets/logo_rose.svg" width={80} height={50} alt="meshdev_logo"/>
         <Image src="/assets/text_logo.svg" width={120} height={80} alt="meshdev_text"/>
@@ -16,6 +26,6 @@ export default function Footer() {
       <p>
         Made with <span className="text-primary"><FontAwesomeIcon icon={faHeart} width={16}/></span> | MeshDev © 2023
       </p>
-    </footer>
+    </motion.footer>
   )
 }
