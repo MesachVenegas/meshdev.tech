86657251e48e9cb732f19082eae8f352214c8023
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-17 07:30:07 -0800

    changes: remove colors and add class dark

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,16 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: "class",
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      primary: "#ee3a6d",
-      secondary: "#ece9e9",
-      black: "#000"
-    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       gridTemplateColumns: {
-        'layout': '120px 1fr'
+        'layout': '100px 1fr'
       }
     },
   },
