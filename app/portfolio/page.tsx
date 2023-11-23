d52cab92937d69132b7e954f8ad89dbe2083ffa6
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-22 17:57:37 -0800

    create: portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,35 @@
+import { encode_sans } from '@/app/libs/fonts';
+import { projects } from '@/app/libs/data';
+import MotionDiv from '@/components/MotionDiv';
+import ProjectCard from '@/components/ProjectCard';
+
 
 export default function PortfolioPage() {
   return (
-    <div>PortfolioPage</div>
+    <main className="flex justify-center items-center py-6 dark:text-white transition-all w-full duration-300 ease-in-out h-full m-auto">
+      <MotionDiv direction="left" duration={.1} styles="w-full min-h-full p-6" >
+        <div className='flex flex-col justify-center items-center gap-6'>
+          <h1 className={`${encode_sans.className} text-4xl  font-medium`} >
+            Proyectos
+          </h1>
+          {/* contenedor de las tarjetas */}
+          <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 m-auto w-full  h-full gap-6 p-6 transition-all'>
+            {/* tarjeta */}
+            {
+              projects.map(({title, description_es, techs, url, links}) => (
+                <ProjectCard
+                  key={title}
+                  title={title}
+                  description={description_es}
+                  techs={ techs }
+                  imgUrl={url}
+                  links={ links }
+                />
+              ))
+            }
+          </div>
+        </div>
+      </MotionDiv>
+    </main>
   )
 }
