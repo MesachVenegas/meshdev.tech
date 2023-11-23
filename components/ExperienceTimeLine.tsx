0354422bf19c7eea23089b9e4487b753c446f234
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-22 17:57:19 -0800

    add: curse python

--- components/ExperienceTimeLine.tsx
+++ components/ExperienceTimeLine.tsx
@@ -15,13 +15,29 @@ export default function ExperienceTimeLine() {
           <Timeline.Point icon={HiCalendar} />
           <Timeline.Content>
             <Timeline.Time className='text-rose-500 dark:text-cyan-400 font-medium'>
-              April 2022
+              Abril 2021 - Junio 2021
             </Timeline.Time>
-            <Timeline.Title className={`${encode_sans.className}`}>
-              E-Commerce UI code in Tailwind CSS
+            <Timeline.Title className={`${encode_sans.className} flex gap-2`}>
+              <Image
+                src="/assets/udemy.svg"
+                width={40}
+                height={40}
+                alt='Academlo logo'
+              />
+              Universidad Python
             </Timeline.Title>
             <Timeline.Body className='text-black/70'>
-              Get started with dozens of web components and interactive elements built on top of Tailwind CSS.
+              Curso de programacion con python desde las bases hasta avanzado, y frameworks para el desarrollo web y la creación de aplicaciones de escritorio con entorno gráfico.
+              <ul className='flex flex-wrap gap-4 mt-2'>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Python</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Tkinter</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>POO</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Django</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Flask</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Jinja</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>PostgreSQL</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>PySide</li>
+              </ul>
             </Timeline.Body>
           </Timeline.Content>
         </Timeline.Item>
@@ -52,12 +68,9 @@ export default function ExperienceTimeLine() {
                 <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>POO</li>
                 <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Vue</li>
                 <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Angular</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Azure</li>
               </ul>
             </Timeline.Body>
-            {/* <Button color="gray">
-              Learn More
-              <HiArrowNarrowRight className="ml-2 h-3 w-3" />
-            </Button> */}
           </Timeline.Content>
         </Timeline.Item>
         <Timeline.Item>
@@ -69,14 +82,14 @@ export default function ExperienceTimeLine() {
             <Timeline.Title className={`${encode_sans.className} flex gap-2 items-center`}>
               <Image
                 src="/assets/academlo_logo.png"
-                width={40}
-                height={40}
+                width={50}
+                height={50}
                 alt='Academlo logo'
               />
               Academlo
             </Timeline.Title>
             <Timeline.Body className='text-black/70'>
-              Desarrollador Full Stack y Ciencias de la computación, Bootcamp para la formacion de desarrolladores Web FullStack.
+              Desarrollador Full Stack y Ciencias de la computación, escuela para la formacion de desarrolladores Web FullStack.
               <ul className='flex flex-wrap gap-4 mt-2'>
                 <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Git</li>
                 <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Github</li>
@@ -96,17 +109,32 @@ export default function ExperienceTimeLine() {
                 <h4>Certificaciones</h4>
                 <ul className='flex flex-wrap items-center justify-start gap-6 mt-2'>
                   <li>
-                    <a href="https://certificates.academlo.com/en/verify/61256560084358" className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>
+                    <a
+                      href="https://certificates.academlo.com/en/verify/61256560084358"
+                      className='flex border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-2 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'
+                      target='_blank'
+                      rel='noopener'
+                    >
                       Fundamentos Desarrollo Web
                     </a>
                   </li>
                   <li>
-                    <a href="https://certificates.academlo.com/en/verify/48509592099651" className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>
+                    <a
+                      href="https://certificates.academlo.com/en/verify/48509592099651"
+                      className='flex border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'
+                      target='_blank'
+                      rel='noopener'
+                  >
                       Aplicaciones Web con React
                     </a>
                   </li>
                   <li>
-                    <a href="https://certificates.academlo.com/en/verify/27296057226977" className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>
+                    <a
+                      href="https://certificates.academlo.com/en/verify/27296057226977"
+                      className='flex border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'
+                      target='_blank'
+                      rel='noopener'
+                    >
                       Back End con NodeJs
                     </a>
                   </li>
@@ -115,7 +143,34 @@ export default function ExperienceTimeLine() {
             </Timeline.Body>
           </Timeline.Content>
         </Timeline.Item>
-        
+        <Timeline.Item>
+          <Timeline.Point icon={HiCalendar} />
+          <Timeline.Content>
+            <Timeline.Time className='text-rose-500 dark:text-cyan-400 font-medium'>
+              Enero 2023 - Abril 2023
+            </Timeline.Time>
+            <Timeline.Title className={`${encode_sans.className} flex gap-2 items-center`} >
+              <Image
+                src="/assets/codefy.png"
+                width={60}
+                height={60}
+                alt='Academlo logo'
+              />
+              Codefy +
+            </Timeline.Title>
+            <Timeline.Body className='text-black/70'>
+              Desarrolladora de software Peruana, desarrollo de sitios web, trabaje en un proyecto para una empresa chilena de consultoria que provee diferentes servicios a sus clientes en asesorías y cursos diversos.
+              <ul className='flex flex-wrap gap-4 mt-2'>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>React</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Github</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>CSS</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>HTML</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>Firebase</li>
+                <li className='border border-slate-500 hover:text-rose-500 hover:border-rose-500  py-1 px-3 rounded-lg dark:hover:border-cyan-400 dark:hover:text-cyan-400 font-medium hover:scale-110 transition-all ease-in-out cursor-pointer'>JavaScript</li>
+              </ul>
+            </Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
       </Timeline>
     </>
   )
