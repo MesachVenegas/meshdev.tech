60efbdb250aa49823b69fea6b1cc0e4963a14319
Mesach Venegas <mesach.venegas@hotmail.com>
2023-11-22 18:05:15 -0800

    create: data constant

---
+++ app/libs/data.ts
@@ -0,0 +1,104 @@
+export const projects = [
+  {
+    url: "/images/Pokedex_preview.webp",
+    title: "Pokedex",
+    description_es: "web app con información de los pokemons existentes",
+    description_en: "web app with information of the pokemons",
+    techs: ['react', 'redux', "javascript", 'css'],
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/MesachVenegas/PokedexWeb"
+      },
+      {
+        name: "Site",
+        url: "https://pokedex-mesach.netlify.app"
+      }
+    ]
+  },
+  {
+    url: "/images/BVS_preview.webp",
+    title: "BVS Advising",
+    description_es: "Website para BVS Advising consultora chilena de diversas ramas",
+    description_en: "Website for BVS Advising, a Chilean consulting company in various branches",
+    techs: ['react', 'redux', "firebase", 'css'],
+    links: [
+      {
+        name: "Github",
+        url: ""
+      },
+      {
+        name: "Site",
+        url: "https://bvsadvising.com"
+      }
+    ]
+  },
+  {
+    url: "/images/Ecommerce_preview.webp",
+    title: "E-commerce Shop",
+    description_es: "Proyecto E-Commerce Academlo, creación de tienda en linea con login y carrito de compras, y en el backend con nodejs, express y sequelize como ORM para trabajar con PostgreSQL como base de datos",
+    description_en: "E-Commerce Academlo project, creation of an online store with login and shopping cart",
+    techs: ['react', 'javascript', "bootstrap", 'node', 'express2','sequelize','postgresql'],
+    links: [
+      {
+        name: "Github",
+        url: "https://github.com/MesachVenegas/Ecommer-React"
+      },
+      {
+        name: "Site",
+        url: "https://ecommer-node.netlify.app"
+      }
+    ]
+  },
+  {
+    url: "/images/paqueteria.webp",
+    title: "Paqueteria 5 Estrellas",
+    description_es: "Proyecto de websita de una paqueteria para la cotización y seguimiento de los envíos de paqueteria.",
+    description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
+    techs: ['react', 'javascript', 'css', 'bootstrap'],
+    links: [
+      {
+        name: "Github",
+        url: ""
+      },
+      {
+        name: "Site",
+        url: "https://www.paqueteria5estrellas.com"
+      }
+    ]
+  },
+  {
+    url: "/images/dashboard.png",
+    title: "Paqueteria 5 Estrellas Dashboard",
+    description_es: "Dashboard y punto de venta para paqueteria 5 estrellas para el control de sus ingresos y ventas",
+    description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
+    techs: ['react', 'typescript', 'next', 'node', 'express2','tailwindcss','postgresql','prisma'],
+    links: [
+      {
+        name: "Github",
+        url: ""
+      },
+      {
+        name: "Site",
+        url: "https://paq-dashboard.vercel.app/dashboard"
+      }
+    ]
+  },
+  {
+    url: "/images/alaniso.png",
+    title: "Genética Cosmica Espiritual",
+    description_es: "Website de Genética Cosmica espiritual, con un blog integrado y e-commerce",
+    description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
+    techs: ['react', 'javascript', 'css', 'next', 'node', 'express2', 'mysql','redux'],
+    links: [
+      {
+        name: "Github",
+        url: ""
+      },
+      {
+        name: "Site",
+        url: "https://alaniso.vercel.app/"
+      }
+    ]
+  },
+]
