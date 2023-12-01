da53413c5f1f98de270c4d4320ccc37255e37754
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-12-01 07:59:20 -0800

    change: size icon

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -23,14 +23,14 @@ export default function ContactPage() {
               <Link
                 href="https://api.whatsapp.com/send?phone=5216631662698&"
                 className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-black bg-green-400 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation">
-                <FontAwesomeIcon icon={faWhatsapp} size='6x' />
+                <FontAwesomeIcon icon={faWhatsapp} size='4x' />
                 Whatsapp
               </Link>
               <Link
                 href="https://www.linkedin.com/in/mesachvenegas/"
                 className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-white bg-blue-600 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation"
               >
-                <FontAwesomeIcon icon={faLinkedin} size='6x' />
+                <FontAwesomeIcon icon={faLinkedin} size='4x' />
                 LinkedInd
               </Link>
             </div>
