936ae31a7093b47eceb6923c7b45a85a3b7ab3d8
MesachVengas <mesach.venegas@hotmail.com>
2024-03-02 01:26:50 -0800

    add anchor to email direct access

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -25,18 +25,18 @@ export default function ContactPage() {
           </h1>
           <div className="flex flex-col w-full h-full justify-center items-center p-6 gap-6">
             <p className="col-span-full p-4">
-              Hola!👋 estare encantado de atenderte, puedes ponerte en contacto con migo a traves de estas redes o bien mediante el formulario, estare en espera de tu mensaje y ansioso por poder empezar a trabajar contigo. Un saludo y nos vemos pronto.
+              Hola!👋 estare encantado de atenderte, puedes ponerte en contacto con migo a traves de estas redes, directamente a esta dirección de correo <a href='mailto: contacto@meshdev.tech' className='text-rose-500 dark:text-cyan-400 italic'>contacto@meshdev.tech</a> o bien mediante el formulario, estare en espera de tu mensaje y ansioso por poder empezar a trabajar contigo. Un saludo y nos vemos pronto.
             </p>
             <div className="flex w-full justify-around">
               <Link
                 href="https://api.whatsapp.com/send?phone=5216631662698&"
-                className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-black bg-green-400 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation">
+                className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-black bg-emerald-500 hover:bg-emerald-400 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation">
                 <FontAwesomeIcon icon={faWhatsapp} size='4x' />
                 Whatsapp
               </Link>
               <Link
                 href="https://www.linkedin.com/in/mesachvenegas/"
-                className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-white bg-blue-600 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation"
+                className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-white bg-blue-600 hover:bg-blue-500 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation"
               >
                 <FontAwesomeIcon icon={faLinkedin} size='4x' />
                 LinkedInd
@@ -45,7 +45,7 @@ export default function ContactPage() {
           </div>
           <div className="flex flex-col gap-4">
             <h2 className="text-center text-xl">Envíame un correo</h2>
-              <ContactForm />
+            <ContactForm />
           </div>
         </div>
 
