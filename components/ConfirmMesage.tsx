143f0bd6f8c8f79ea0eea8c14436114da7d46e0a
MesachVengas <mesach.venegas@hotmail.com>
2024-03-02 01:25:54 -0800

    change message and data display

--- components/ConfirmMesage.tsx
+++ components/ConfirmMesage.tsx
@@ -3,7 +3,13 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
-const ConfirmMessage = () => {
+
+interface DataProps {
+  data: { id: string, message: string};
+  error?: { name: string, statusCode: number, message: string }
+}
+
+const ConfirmMessage = ({ data, error } : DataProps ) => {
   const router = useRouter();
   const [isOpen, setIsOpen] = useState<boolean>(true);
 
@@ -12,15 +18,20 @@ const ConfirmMessage = () => {
     router.push("/");
   };
 
+
   return (
     <div>
       {
         isOpen && (
-          <div className="fixed inset-0 flex items-center justify-center">
+          <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex items-center justify-center">
             <div className="bg-slate-100 dark:bg-slate-900 p-8 rounded-lg shadow-lg">
-              <h2 className="text-xl font-bold mb-4">Correo enviado</h2>
-              <p>Tu correo ha sido enviado correctamente.</p>
-              <button onClick={closeModal} className="mt-4 bg-cyan-400 text-black px-4 py-2 rounded">
+              <h2 className="text-xl font-bold mb-4">{ error ? `${error.name} ${error.statusCode}` : "Correo enviado" }</h2>
+              <p>{error ? error.message : data.message}</p>
+              <button
+                type="button"
+                onClick={closeModal}
+                className="mt-4 bg-cyan-400 text-black px-4 py-2 rounded"
+              >
                 Cerrar
               </button>
             </div>
