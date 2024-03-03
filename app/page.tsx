e6d6b6e78f9ab968357cd1b3b18871024fbed488
MesachVengas <mesach.venegas@hotmail.com>
2024-03-02 17:00:20 -0800

    change padding on mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -19,22 +19,23 @@ export default function Home() {
   return (
     <>
         <Cover />
-        <main className="flex justify-center items-center p-6 w-full h-screen dark:bg-slate-900 dark:text-white transition-all duration-300 ease-in-out">
+        <main className="flex justify-center items-center p-0 lg:p-6 w-full max-w-[1580px] mx-auto h-screen dark:bg-slate-900 dark:text-white transition-all duration-300 ease-in-out">
           <motion.div
-            className="flex items-center justify-around bg-white/30 dark:bg-slate-950/30 w-full h-full shadow-xl rounded-md  backdrop-blur-sm md:px-6 py-2"
+            className="flex flex-col-reverse lg:flex-row items-center justify-around bg-white/30 dark:bg-slate-950/30 w-full h-full shadow-xl rounded-md  backdrop-blur-sm px-4 md:px-6 py-2"
             variants={ fadeIn('left', 0.1) }
             initial="hidden"
             animate="show"
             exit="hidden"
           >
-            <div className="flex flex-col gap-2">
-              <h1 className={`${encode_sans.className} antialiased text-2xl md:text-3xl xl:text-4xl font-bold `}>
-                Hola! <br /> Soy <span className="text-rose-500 text-4xl md:text-6xl">Mesach Venegas</span>
+            <div className="flex flex-col gap-6 justify-center items-center lg:items-start w-full  xl:max-w-2xl p-2">
+              <h1 className={`${encode_sans.className} antialiased text-3xl text-center lg:text-left md:text-4xl xl:text-5xl font-bold space-y-2`}>
+                Hola! <br /> Soy <span className="text-rose-500 text-5xl md:text-7xl">Mesach Venegas</span>
               </h1>
               <div className="text-2xl font-bold tracking-wide">
                 <TechWriter />
               </div>
               <SocialLinks />
+
               <div className="flex flex-wrap gap-3 justify-around mt-8 xl:w-[400px]">
                 <a
                   className="flex gap-2 items-center border-2 font-bold py-3 px-2 xl:py-4 xl:px-6 border-black dark:border-white rounded-md hover:border-rose-500 dark:hover:border-rose-500 hover:text-rose-500 hover:shadow-[0_10px_10px_rgba(0,0,0,.6)] transition-all ease-in-out dark:hover:shadow-[0_10px_10px_rgba(225,50,50,.8)]"
@@ -50,7 +51,8 @@ export default function Home() {
                 </Link>
               </div>
             </div>
-            <div className="hidden lg:block">
+
+            <div className="">
               <Image
                 className="drop-shadow-[0_25px_10px_rgba(0,0,0,.8)] floating-animation"
                 src="/images/hero.png"
