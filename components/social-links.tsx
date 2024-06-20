729a993a6b42b5e4b231b63f6bef3257c8d4e85b
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-20 16:35:38 -0700

    changes: use cn to change classname variancy

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -2,22 +2,31 @@ import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { cn } from "@/lib/utils";
 import { socialsLinks } from "@/lib/iconsMenus";
 
-export default function SocialLinks({ isFooter }: { isFooter?: boolean}) {
+export default function SocialLinks({ isFooter, className }: { isFooter?: boolean, className?: string}) {
   return (
-    <div className="flex justify-between w-[250px] ">
-        {
-          socialsLinks.map( ({name, icon, url}) => (
-            <span key={name} className="hover:text-rose-500 w-6 h-6">
-              <Link href={url} className="menu-icon" target="_blank" title={`${name} link icon`}>
-                {
-                  isFooter ? <FontAwesomeIcon icon={icon} size="xl" /> : <FontAwesomeIcon icon={icon} size="2xl" />
-                }
-              </Link>
-            </span>
-          ))
-        }
-      </div>
-  )
+		<div
+			className={cn(
+				`flex gap-8 w-[250px] ${isFooter ? "justify-center" : "justify-start"}`,
+        className
+			)}>
+			{socialsLinks.map(({ name, icon, url }) => (
+				<span key={name} className="hover:text-primary w-6 h-6">
+					<Link
+						href={url}
+						className="menu-icon"
+						target="_blank"
+						title={`${name} link icon`}>
+						{isFooter ? (
+							<FontAwesomeIcon icon={icon} size="xl" />
+						) : (
+							<FontAwesomeIcon icon={icon} size="2xl" />
+						)}
+					</Link>
+				</span>
+			))}
+		</div>
+	);
 }
