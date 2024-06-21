7a2c2e39a964e274f7426437b1d4d59e1f6a493c
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-21 00:29:29 -0700

    styles:

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -13,30 +13,29 @@ export const metadata: Metadata = {
 
 export default function PortfolioPage() {
   return (
-    <main className="flex justify-center items-center p-0 py-6 lg:p-6 dark:text-white transition-all w-full duration-300 ease-in-out h-full m-auto">
-      <MotionDiv direction="left" duration={.1} className="flex items-center justify-around bg-white/30 dark:bg-slate-950/30 w-full h-full shadow-xl rounded-md  backdrop-blur-sm md:px-6 py-2" >
-        <div className='flex flex-col justify-center items-center gap-6'>
-          <h1 className={`${encode_sans.className} text-4xl  font-medium`} >
-            Proyectos
-          </h1>
-          {/* contenedor de las tarjetas */}
-          <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 m-auto w-full  h-full gap-6 p-6 transition-all'>
-            {/* tarjeta */}
-            {
-              projects.map(({title, description_es, techs, url, links}) => (
+		<main className=" container flex justify-center items-center p-0 py-6 lg:p-6 transition-all w-full duration-300 ease-in-out h-full m-auto">
+			<MotionDiv
+				direction="left"
+				duration={0.1}
+				className="flex items-center justify-around bg-base-100/50 w-full h-full shadow-xl rounded-md  backdrop-blur-sm md:px-6 py-2">
+				<div className="flex flex-col justify-center items-center gap-6">
+					<h1 className={`${encode_sans.className} text-4xl  font-medium`}>
+						Proyectos
+					</h1>
+					{/* contenedor de las tarjetas */}
+					<div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 m-auto w-full  h-full gap-6 p-6 transition-all">
+						{/* tarjeta */}
+						{
+              projects.map( project => (
                 <ProjectCard
-                  key={title}
-                  title={title}
-                  description={description_es}
-                  techs={techs}
-                  imgUrl={url}
-                  links={ links }
+                  key={project.url}
+                  {...project}
                 />
               ))
             }
-          </div>
-        </div>
-      </MotionDiv>
-    </main>
-  )
+					</div>
+				</div>
+			</MotionDiv>
+		</main>
+	);
 }
