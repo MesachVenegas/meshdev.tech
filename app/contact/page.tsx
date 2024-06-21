4a8dcccd8952372ef6cafc100acfab3bc782fc50
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-21 00:29:52 -0700

    changes: text data of contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -16,39 +16,66 @@ export const metadata: Metadata = {
 export default function ContactPage() {
 
   return (
-    <main className="flex w-full h-full justify-center items-center py-6 dark:text-white transition-all duration-300 ease-in-out m-auto">
-      <MotionDiv direction="left" duration={.1} className="w-full max-w-7xl min-h-full p-6">
-        <div className="grid grid-cols-1 lg:grid-cols-2 w-full h-full gap-6">
-          <h1 className={`${encode_sans.className} text-4xl font-medium col-span-full text-center`}>
-            Contacta me
-          </h1>
-          <div className="flex flex-col w-full h-full justify-center items-center p-6 gap-6">
-            <p className="col-span-full p-4">
-              Hola!👋 estare encantado de atenderte, puedes ponerte en contacto con migo a traves de estas redes, directamente a esta dirección de correo <a href='mailto: contacto@meshdev.tech' className='text-rose-500 dark:text-cyan-400 italic'>contacto@meshdev.tech</a> o bien mediante el formulario, estare en espera de tu mensaje y ansioso por poder empezar a trabajar contigo. Un saludo y nos vemos pronto.
-            </p>
-            <div className="flex w-full justify-around">
-              <Link
-                href="https://api.whatsapp.com/send?phone=5216631662698&"
-                className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-black bg-emerald-500 hover:bg-emerald-400 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation">
-                <FontAwesomeIcon icon={faWhatsapp} size='4x' />
-                Whatsapp
-              </Link>
-              <Link
-                href="https://www.linkedin.com/in/mesachvenegas/"
-                className="flex flex-col w-36 h-36 md:w-44 md:h-44 justify-center items-center text-white bg-blue-600 hover:bg-blue-500 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation"
-              >
-                <FontAwesomeIcon icon={faLinkedin} size='4x' />
-                LinkedInd
-              </Link>
-            </div>
-          </div>
-          <div className="flex flex-col gap-4">
-            <h2 className="text-center text-xl">Envíame un correo</h2>
-            <ContactForm />
-          </div>
-        </div>
-
-      </MotionDiv>
-    </main>
-  )
+		<main className="flex w-full h-full justify-center items-center py-6 transition-all duration-300 ease-in-out m-auto">
+			<MotionDiv
+				direction="left"
+				duration={0.1}
+				className="w-full max-w-7xl min-h-full p-6">
+				<div className="grid grid-cols-1 lg:grid-cols-2 w-full h-full gap-6">
+					<h1
+						className={`${encode_sans.className} text-4xl font-medium col-span-full text-center`}>
+						Contacta me
+					</h1>
+					<div className="mockup-window border border-accent">
+						<div className=" flex flex-col w-full h-full justify-center items-center p-6 gap-6 bg-base-300">
+							<div className="chat chat-start w-full">
+								<div className="chat-bubble shadow-solid">Hola!👋</div>
+							</div>
+							<div className="chat chat-start w-full">
+								<div className="chat-bubble shadow-solid">
+									Puedes ponerte en contacto conmigo mediante mi correo
+									electrónico o redes
+								</div>
+							</div>
+							<div className="chat chat-end w-full">
+								<div className="chat-bubble shadow-solid">
+									Puedes enviar un correo a{" "}
+									<a
+										href="mailto: contacto@meshdev.tech"
+										className="text-accent italic">
+										contacto@meshdev.tech
+									</a>{" "}
+									o llenando el formulario
+								</div>
+							</div>
+							<div className="chat chat-start w-full">
+								<div className="chat-bubble shadow-solid">
+									o bien en cualquiera de los siguiente medios
+								</div>
+							</div>
+              <div className='divider divider-accent'></div>
+							<div className="flex w-full justify-around">
+								<Link
+									href="https://api.whatsapp.com/send?phone=5216631662698&"
+									className="flex flex-col w-20 h-20 md:w-32 md:h-32 justify-center items-center text-black bg-emerald-500 hover:bg-emerald-400 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation">
+									<FontAwesomeIcon icon={faWhatsapp} size="4x" />
+									Whatsapp
+								</Link>
+								<Link
+									href="https://www.linkedin.com/in/mesachvenegas/"
+									className="flex flex-col w-20 h-20 md:w-32 md:h-32 justify-center items-center text-white bg-blue-600 hover:bg-blue-500 shadow-2xl rounded-lg gap-2 cursor-pointer transition-all duration-300 ease-in-out floating-animation">
+									<FontAwesomeIcon icon={faLinkedin} size="4x" />
+									LinkedInd
+								</Link>
+							</div>
+						</div>
+					</div>
+					<div className="flex flex-col gap-4">
+						<h2 className="text-center text-xl">Envíame un correo</h2>
+						<ContactForm />
+					</div>
+				</div>
+			</MotionDiv>
+		</main>
+	);
 }
