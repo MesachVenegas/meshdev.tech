a3b2664671d9e3a541839c8adec02049ed28d66a
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2024-06-21 00:28:19 -0700

    changes: project data

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,70 +1,92 @@
 export const projects = [
   {
-    url: "/images/BVS_preview.webp",
+    image: "/images/BVS_preview.webp",
+    url: 'https://bvsadvising.com',
     title: "BVS Advising",
     description_es: "Website para BVS Advising consultora chilena de diversas ramas",
     description_en: "Website for BVS Advising, a Chilean consulting company in various branches",
     techs: ['react', 'redux', "firebase", 'css'],
     links: [
       {
-        name: "Github",
+        name: "git",
         url: ""
       },
       {
-        name: "Site",
-        url: "https://bvsadvising.com"
-      }
+        name: "figma",
+        url: ""
+      },
     ]
   },
   {
-    url: "/images/paqueteria.webp",
+    image: "/images/paqueteria_demo.png",
+    url: "https://www.paqueteria5estrellas.com",
     title: "Paqueteria 5 Estrellas",
-    description_es: "Proyecto de websita de una paqueteria para la cotización y seguimiento de los envíos de paqueteria.",
+    description_es: "Website para la paqueteria con el fin de poder proporcionar a sus clientes la cotización de los envíos, asi como proporcionar un sistema de seguimiento de sus envíos y anuncios.",
     description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
-    techs: ['react', 'javascript', 'css', 'bootstrap'],
+    techs: ['react', 'typescript', 'next', 'tailwindcss', 'node', 'mongodb', 'prisma'],
     links: [
       {
-        name: "Github",
+        name: "git",
         url: ""
       },
       {
-        name: "Site",
-        url: "https://www.paqueteria5estrellas.com"
-      }
+        name: "figma",
+        url: ""
+      },
     ]
   },
   {
-    url: "/images/dashboard.png",
+    url: "https://paq-dashboard.vercel.app/dashboard",
+    image: "/images/dashboard.png",
     title: "Paqueteria 5 Estrellas Dashboard",
     description_es: "Dashboard y punto de venta para paqueteria 5 estrellas para el control de sus ingresos y ventas",
     description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
-    techs: ['react', 'typescript', 'next', 'node', 'express2', 'tailwindcss', 'mysql', 'prisma'],
+    techs: ['react', 'typescript', 'next', 'node', 'tailwindcss', 'mysql', 'prisma'],
     links: [
       {
-        name: "Github",
+        name: "git",
         url: ""
       },
       {
-        name: "Demo",
-        url: "https://paq-dashboard.vercel.app/dashboard"
-      }
+        name: "figma",
+        url: ""
+      },
     ]
   },
   {
-    url: "/images/alaniso.png",
-    title: "Genética Cosmica Espiritual",
-    description_es: "Website de Genética Cosmica espiritual, con un blog integrado y e-commerce con paypal para la pasantía de pagos",
+    url: "https://kazu-moda.vercel.app/",
+    image: "/images/kazu_moda.png",
+    title: "Kazu Moda Circular",
+    description_es: "Tienda electronica para la reventa de artículos de ropa de segunda mano, que busca la reutilización de las mismas y de esta manera ayudar al medio ambiente.",
     description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
-    techs: ['react', 'javascript', 'css', 'next', 'node', 'express2', 'mysql', 'redux', 'paypal'],
+    techs: ['react', 'javascript', 'tailwindcss', 'next', 'express2', 'node', 'mongodb', 'redux'],
     links: [
       {
-        name: "Github",
-        url: ""
+        name: "git",
+        url: "https://github.com/No-Country/c17-19-n-node-react"
       },
       {
-        name: "Demo",
-        url: "https://alaniso.vercel.app/"
-      }
+        name: "figma",
+        url: "https://www.figma.com/design/TttpMU5IVpUcC8W45Wtren/ECOMMERCE---MODA-CIRCULAR?node-id=1430-6262&t=JMKw0ij13nFshlrx-0"
+      },
+    ]
+  },
+  {
+    image: '/images/et_demo.png',
+    url: "https://s15-07-n-node-react.vercel.app/",
+    title: "ET: Expense Tracker",
+    description_es: "Webapp para el control de gastos, con un sistema de autenticación, gestión de categorías y un sistema de notificación.",
+    description_en: "Website project for a parcel company for quoting and tracking parcel shipments.",
+    techs: ['react', 'typescript', 'tailwindcss', 'next', 'node', 'mongodb', 'redux'],
+    links: [
+      {
+        name: "git",
+        url: "https://github.com/MesachVenegas/s15-07-n-node-react"
+      },
+      {
+        name: "figma",
+        url: "https://www.figma.com/design/Lb4pok0BJCbn0KROiayozj/Personal-Finance---Safeflow-(s-15-07-n-node-react)?node-id=10-131&t=JuTZLxocOBDU5EtG-0"
+      },
     ]
   },
 ]
